1d5a16d48907d022ecc186830659d2adf26075a6
Dmitrii Ubskii <ubskydm@gmail.com>
2020-12-31 23:40:15 +0300

    Message line in Display

--- src/main.py
+++ src/main.py
@@ -30,6 +30,7 @@ class Display:
         curses.start_color()
         self._board_display = self._stdscr.subwin(8, 16, 1, 2)
         self._input_line = self._stdscr.subwin(1, 19, 11, 0)
+        self._messages = self._stdscr.subwin(1, 19, 13, 0)
 
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
@@ -56,6 +57,11 @@ class Display:
         self._input_line.clear()
         self._input_line.addstr(0, 0, '> ')
         return self._input_line.getstr(6).decode('utf-8')
+
+    def show_message(self, message: str):
+        self._messages.clear()
+        self._messages.addstr(0, 0, message)
+        self._messages.refresh()
 
 
 if __name__ == '__main__':
