34b1000ddeb62713aac43c448ad4e8dd8e9a3100
Dmitrii Ubskii <ubskydm@gmail.com>
2021-01-06 23:36:27 +0300

    Revert __threatens() to original pseudo-legality check

--- src/board.py
+++ src/board.py
@@ -371,18 +371,15 @@ class Board:
             moves |= {Move(piece=piece, src=Square(i)._square_name, dest=s._square_name) for s in self.legal_moves(Square(i))}
         return moves
 
-    def __threatens_check(self, src: Square, dest: Square) -> bool:
+    def __threatens(self, src: Square, dest: Square) -> bool:
         if self[src] == '.':
             return False
+        if get_piece_owner(self[src]) == get_piece_owner(self[dest]):
+            return False  # can't capture own pieces
 
         piece = self[src]
         src_owner = get_piece_owner(piece)
         piece = piece.upper()
-
-        king = format_piece('K', get_opponent(src_owner))
-        king_square = Square(self._board.index(king))
-        xray_board = copy.deepcopy(self)
-        xray_board[king_square] = '.'
 
         if piece == 'K':
             return src.dist(dest) == 1
@@ -411,7 +408,7 @@ class Board:
             dv = (sgn(dest._file - src._file), sgn(dest._rank - src._rank))
             next_square = src + dv
             while next_square != dest:
-                if xray_board[next_square] != '.':  # obstacle
+                if self[next_square] != '.':  # obstacle
                     return False
                 next_square += dv
             return True  # got to dest with no obstacles
@@ -424,7 +421,7 @@ class Board:
         for i, piece in enumerate(self._board):
             if piece == '.' or get_piece_owner(piece) != player:
                 continue
-            if self.__threatens_check(Square(i), square):
+            if self.__threatens(Square(i), square):
                 attackers.add(Square(i))
         return len(attackers) > 0
 
@@ -434,7 +431,7 @@ class Board:
         for i, piece in enumerate(self._board):
             if piece == '.' or get_piece_owner(piece) == owner:
                 continue
-            if self.__threatens_check(Square(i), square):
+            if self.__threatens(Square(i), square):
                 attackers.add(Square(i))
         return attackers
 
