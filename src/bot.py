7ada29aedd7da90d2db4df48fe711f0ab2b9db4b
Dmitrii Ubskii <ubskydm@gmail.com>
2021-01-07 09:27:27 +0300

    Simple 1-ply aggressive bot

--- src/bot.py
+++ src/bot.py
@@ -3,7 +3,24 @@
 import random
 
 from board import Board, Move
+from board import get_piece_owner
 
 
 def random_bot(board: Board) -> Move:
     return random.choice(list(board.get_all_legal_moves()))
+
+
+def dummy_bot(board: Board) -> Move:
+    PIECE_VALUES = {'P': 1, 'N': 3, 'B': 3, 'R': 5, 'Q': 9, 'K': 100}
+    def eval_board(board: Board) -> int:
+        mobility_advantage = len(board.get_all_legal_moves()) - len(board._flip_player_copy().get_all_legal_moves())
+        piece_advantage = 0
+        for piece in board._board:
+            if piece == '.':
+                continue
+            if get_piece_owner(piece) == board._active_player:
+                piece_advantage += PIECE_VALUES[piece.upper()]
+            else:
+                piece_advantage -= PIECE_VALUES[piece.upper()]
+        return mobility_advantage + piece_advantage
+    return min(board.get_all_legal_moves(), key=lambda m: eval_board(board.make_move_copy(m)))
