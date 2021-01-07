9c08c8a7b617afd62cb0bea69ab070ce6bd1adb4
Dmitrii Ubskii <ubskydm@gmail.com>
2021-01-07 09:06:05 +0300

    dummy_bot > random_bot (more appropriate name)

--- src/bot.py
+++ src/bot.py
@@ -4,5 +4,5 @@ import random
 
 from board import Board, Move
 
-def dummy_bot(board: Board) -> Move:
+def random_bot(board: Board) -> Move:
     return random.choice(list(board.get_all_legal_moves()))
